d1aaf8d6fcd8db13b1fe165c383cc6c3a9157701
Lachlan Chambers <67294530+Ardbit@users.noreply.github.com>
2020-11-17 09:37:50 +1000

    Switched to Netlify.

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,11 +18,6 @@ export default {
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
         ],
-        script: [
-            { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-2SDYVTG6YM' },
-            { src: '/gtag.js' },
-            { src: '/status.js' }
-        ]
     },
     server: {
         port: parseInt(process.env.PORT),
