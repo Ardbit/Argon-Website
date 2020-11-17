82d0280e83f76cf0b42a7b96f0028a4634016749
Lachlan Chambers <67294530+Ardbit@users.noreply.github.com>
2020-11-18 09:38:34 +1000

    Changed from Nuxt.js to AdonisJs Preview

---
+++ public/global.js
@@ -0,0 +1,10 @@
+$(window).ready(function () {
+  Particles.init({
+    selector: '.background',
+    maxParticles: 80,
+    sizeVariations: 0,
+    speed: 0.25,
+    color: '#FFFFFF',
+    connectParticles: true
+  })
+});
